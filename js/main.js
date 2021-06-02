aa76bd0f458c4cc1e460af4712c1a43c6777b598
Oleg Tkachenko <oleg@elsci.io>
2021-06-02 12:12:21 +0300

    Rid of JQuery lib

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
-$(document).ready(function(){
-  $("a").on('click', function(event) {
-    if (this.hash !== "") {
-      event.preventDefault();
-      var hash = this.hash;
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-        window.location.hash = hash;
-      });
-    } 
-  });
+'use strict'
+
+document.addEventListener("DOMContentLoaded",  () => {
+    document.querySelectorAll('a').forEach(a => {
+        a.addEventListener('click', (evt) => {
+            let elementId = a.hash;
+            if (elementId.length){
+                evt.preventDefault();
+                let element = document.querySelector(elementId);
+                let top = element.getBoundingClientRect().top;
+                window.scrollTo({top: top, behavior: 'smooth'})
+                setTimeout(() => {
+                    window.location.hash = elementId;
+                }, 800)
+            }
+        })
+    })
 });
