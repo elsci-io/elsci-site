5d6ce369ae5f2eb2f280137755de547ca51c41e6
Stanislav Bashkyrtsev <stas@elsci.io>
2023-03-28 12:31:13 +0400

    Update docker installation procedure to install `docker compose`

--- files/peaksel/install_docker.sh
+++ files/peaksel/install_docker.sh
@@ -1,19 +1,20 @@
 set -ex
 
-install="apt-get install -y"
-update="apt-get update"
+install="apt install -y"
+update="apt update"
+${update}
 
-${install} apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
+${install} ca-certificates curl gnupg
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 ${update}
-${install} docker-ce docker-ce-cli containerd.io docker-compose
+${install} docker-ce docker-ce-cli containerd.io  docker-compose-plugin
+systemctl enable docker
+service docker start
