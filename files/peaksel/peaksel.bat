8668c3ce0be242eb7ff969e8eda6db5cdc04c51f
Stanislav Bashkyrtsev <stas@elsci.io>
2023-03-29 09:23:44 +0400

    Add users to the list of variables peaksel.bat

--- files/peaksel/peaksel.bat
+++ files/peaksel/peaksel.bat
@@ -1,7 +1,23 @@
 @echo off
 set PGPASSWORD=password
 set PG_HOME=C:\Program Files\PostgreSQL\14
-set JAVA_HOME=openjdk-17_windows-x64_bin\jdk-17
+set JAVA_HOME=openjdk-19_windows-x64_bin\jdk-19
+
+set db.url=jdbc:postgresql://localhost:5432/elsci?currentSchema=peaksel
+set db.user=peaksel
+set db.pass=HuCZSQUH
+set user.log_dir=log
+
+set security.in_memory_users.name=user
+set security.in_memory_users.password=user
+set security.in_memory_users.role=USER
+
+:: In case you want to enable authentication with OAuth2 + OpenID Connect
+::set security.oauth2.enabled='true'
+::set security.oauth2.authorization-uri=https://...
+::set security.oauth2.token-uri=https://...
+::set security.oauth2.user-info-uri=https://...
+::set security.oauth2.client-id=...
 
 if exist init.sql.done (
     echo "DB already exists, continue.."
@@ -10,8 +26,4 @@ if exist init.sql.done (
     copy NUL init.sql.done
 )
 
-set db.url=jdbc:postgresql://localhost:5432/elsci?currentSchema=peaksel
-set db.user=peaksel
-set db.pass=HuCZSQUH
-set user.log_dir=log
 %JAVA_HOME%\bin\java -jar peaksel.jar
