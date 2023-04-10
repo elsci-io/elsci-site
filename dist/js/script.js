ba6d593369614f48428dc46475c6f8b70df316ac
Kateryna Tkachenko <es.tka4enko@gmail.com>
2023-04-10 19:16:18 +0300

    add header

---
+++ dist/js/script.js
@@ -0,0 +1,1 @@
+document.querySelector(".header-elsci__menu-burger").addEventListener("click",(()=>{document.querySelector(".header-elsci__menu-burger").classList.toggle("active"),document.querySelector(".header-elsci__navigation").classList.toggle("active")}));
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
+let fs = require('fs');
+
+const header = fs.readFileSync(__dirname + '/src/header.html');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            header: header
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'src/css', to: 'css' },
+                { from: 'src/js', to: 'js' },
+                { from: 'src/images', to: 'images' }
+            ]
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use:
+                    [
+                        'style-loader',
+                        'css-loader',
+                    ]
+            }
+        ]
+    },
+    devServer: {
+        static: path.resolve(__dirname, '..'),
+        historyApiFallback: true,
+        hot: true,
+        host: 'localhost',
+        port: 3457
+    }
+};
