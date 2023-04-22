5ed7ba728cf50223194efa4b525398b192bbd387
Kateryna Tkachenko <es.tka4enko@gmail.com>
2023-04-22 22:26:13 +0300

    Added active class to header link

--- dist/js/script.js
+++ dist/js/script.js
@@ -6,3 +6,6 @@ document.querySelector(".header-elsci__menu-burger").addEventListener("click", (
             .querySelector(".header-elsci__navigation")
             .classList.toggle("active");
 });
+
+const pageName = document.querySelector('main').dataset.page;
+document.querySelector(`.header-elsci__link[data-page="${pageName}"]`).classList.add("active");
