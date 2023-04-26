50107e6253e666f54265a859a360400b60995396
Kateryna Tkachenko <es.tka4enko@gmail.com>
2023-04-26 13:47:23 +0300

    Better header

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,13 @@
-document.querySelector(".header-elsci__menu-burger").addEventListener("click", () => {
-        document
-            .querySelector(".header-elsci__menu-burger")
-            .classList.toggle("active");
-        document
-            .querySelector(".header-elsci__navigation")
+document.querySelector(".header__burger").addEventListener("click", () => {
+    document
+        .querySelector(".header__burger")
         .classList.toggle("active");
-    document.querySelector("body").classList.toggle("lock")
+    document
+        .querySelector(".header-elsci__navigation")
+        .classList.toggle("active");
+    // document.querySelector(".wrapper").classList.toggle("lock");
+    document.body.classList.toggle("lock");
 });
 
 const pageName = document.querySelector('main').dataset.page;
-document.querySelector(`.header__link[data-page="${pageName}"]`).classList.add("active");
+document.querySelector(`.navigation-list__item[data-page="${pageName}"]`).classList.add("active");
