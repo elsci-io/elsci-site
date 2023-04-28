54e66d9310f659639f6a33a2442ffabb1d174c0c
Kateryna Tkachenko <es.tka4enko@gmail.com>
2023-04-28 13:41:01 +0300

    Added link to our products in menu

--- dist/js/script.js
+++ dist/js/script.js
@@ -12,22 +12,8 @@ window.addEventListener('scroll', (el) => {
     document.querySelector('.header-elsci').classList.toggle('scroll', document.scrollingElement.scrollTop != 0)
 })
 
-// const pageName = document.querySelector('main').dataset.page;
-// document.querySelector(`.navigation-list__item[data-page="${pageName}"]`).classList.add("active");
-
-function crossWord(id) {
-    document.querySelector(`.word-box[data-id="${id}"] .wrong`).classList.add('strike');
+const pageName = document.querySelector('main').dataset.page;
+if (pageName) {
+    document.querySelector(`.navigation-list__item[data-page="${pageName}"]`).classList.add("active");
 }
 
-function showWord(id) {
-    document.querySelector(`.word-box[data-id="${id}"] .correct`).classList.add('visible');
-}
-
-for (let id = 1; id <= 7; id++) {
-    setTimeout(() => {
-        crossWord(id)
-    }, id * 2000)
-    setTimeout(() => {
-        showWord(id)
-    }, ((id * 2000) + 2000))
-}
