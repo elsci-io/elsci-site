879436cc6607c69140c52af354116f39fe47556d
Kateryna Tkachenko <es.tka4enko@gmail.com>
2023-05-05 14:17:17 +0300

    Add icon external link

--- src/js/script.js
+++ src/js/script.js
@@ -16,16 +16,14 @@ document.querySelector('.header__link--bookmarks').addEventListener('click', () 
         .querySelector(".header-elsci__navigation")
         .classList.remove("active");
     document.body.classList.remove("lock");
-})
+});
 
 window.addEventListener('scroll', (el) => {
     document.querySelector('.header-elsci').classList.toggle('scroll', document.scrollingElement.scrollTop != 0);
     if (document.querySelector('.header-peaksel')) {
         document.querySelector('.header-peaksel').classList.toggle('scroll', document.scrollingElement.scrollTop != 0)
     }
-})
-
-
+});
 
 const pageName = document.querySelector('main').dataset.page;
 console.log(pageName);
