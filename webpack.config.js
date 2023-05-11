4b06aaecbb44a2120486ee774c33b1589c4fc9d1
Oleg Tkachenko <oleg@elsci.io>
2023-05-11 20:30:17 +0200

    cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -4,68 +4,68 @@ module.exports = {
     plugins: [
         new HtmlBundlerPlugin({
             entry: {
-                'index': { // => dist/index.html (key is output filename w/o '.html')
+                'index': { // => target/index.html (key is output filename w/o '.html')
                     import: './src/index.html', // template file
                 },
-                'business-model': { // => dist/index.html (key is output filename w/o '.html')
-                    import: './src/business-model.html', // template file
+                'business-model': {
+                    import: './src/business-model.html',
                 },
-                'about': { // => dist/index.html (key is output filename w/o '.html')
-                    import: './src/about.html', // template file
+                'about': {
+                    import: './src/about.html',
                 },
                 'peaksel/index': {
-                    import: './src/peaksel/html/index.html', // template file
+                    import: './src/peaksel/html/index.html',
                 },
                 'peaksel/docs': {
-                    import: './src/peaksel/html/docs.html', // template file
+                    import: './src/peaksel/html/docs.html',
                 },
                 'peaksel/buy': {
-                    import: './src/html/peaksel/buy.html', // template file
+                    import: './src/html/peaksel/buy.html',
                 },
                 'peaksel/docs/add-substance': {
-                    import: './src/peaksel/html/docs/add-substance.html', // template file
+                    import: './src/peaksel/html/docs/add-substance.html',
                 },
                 'peaksel/docs/align-detectors': {
-                    import: './src/peaksel/html/docs/align-detectors.html', // template file
+                    import: './src/peaksel/html/docs/align-detectors.html',
                 },
                 'peakseldocs/batch-navigation': {
-                    import: './src/peaksel//html/docs/batch-navigation.html', // template file
+                    import: './src/peaksel//html/docs/batch-navigation.html',
                 },
                 'peaksel/docs/import-automation': {
-                    import: './src/peaksel/html/docs/import-automation.html', // template file
+                    import: './src/peaksel/html/docs/import-automation.html',
                 },
                 'peaksel/docs/import-manual-upload': {
-                    import: './src/peaksel/html/docs/import-manual-upload.html', // template file
+                    import: './src/peaksel/html/docs/import-manual-upload.html',
                 },
                 'peaksel/docs/peaks': {
-                    import: './src/peaksel/html/docs/peaks.html', // template file
+                    import: './src/peaksel/html/docs/peaks.html',
                 },
                 'peaksel/docs/reports': {
-                    import: './src/peaksel/html/docs/reports.html', // template file
+                    import: './src/peaksel/html/docs/reports.html',
                 },
                 'peaksel/docs/vis-templates': {
-                    import: './src/peaksel/html/docs/vis-templates.html', // template file
+                    import: './src/peaksel/html/docs/vis-templates.html',
                 },
                 'peaksel/docs/vocabulary': {
-                    import: './src/peaksel/html/docs/vocabulary.html', // template file
+                    import: './src/peaksel/html/docs/vocabulary.html',
                 },
                 'peaksel/docs/waters-structures': {
-                    import: './src/peaksel/html/docs/waters-structures.html', // template file
+                    import: './src/peaksel/html/docs/waters-structures.html',
                 },
                 'peaksel/docs/zoom': {
-                    import: './src/peaksel/html/docs/zoom.html', // template file
+                    import: './src/peaksel/html/docs/zoom.html',
                 },
                 'peaksel/article/hte-qc-peaks': {
-                    import: './src/peaksel/html/article/hte-qc-peaks.html', // template file
+                    import: './src/peaksel/html/article/hte-qc-peaks.html',
                 },
                 'peaksel/article/mass-spec': {
-                    import: './src/peaksel/html/article/mass-spec.html', // template file
+                    import: './src/peaksel/html/article/mass-spec.html',
                 },
                 'peaksel/article/wl-extraction': {
-                    import: './src/peaksel/html/article/wl-extraction.html', // template file
+                    import: './src/peaksel/html/article/wl-extraction.html',
                 },
                 'molevent/index': {
-                    import: './src/molevent/html/index.html', // template file
+                    import: './src/molevent/html/index.html',
                 },
             },
             js: {
