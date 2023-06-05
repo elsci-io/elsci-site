228bab53dabea4b37efe6913e589dc05ff2d0bbd
Kateryna Tkachenko <es.tka4enko@gmail.com>
2023-06-05 12:43:52 +0300

    Close popup by esc

--- src/js/popup.js
+++ src/js/popup.js
@@ -26,6 +26,12 @@ images.forEach(image => {
 
 popup.addEventListener('click', (event) => zoomOut(event));
 
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+        zoomOut(event);
+    }
+});
+
 
 // Functions that zoom/unzoom the image
 
